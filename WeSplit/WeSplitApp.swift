15aa66a2f0d1a628e38cd2216d59814ae893ecc3
thermaldeveloper <thermaldeveloper@icloud.com>
2025-03-23 11:37:56 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Brad Rice on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
